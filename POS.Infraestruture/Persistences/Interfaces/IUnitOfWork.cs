8869313df0766cc3fe995837474b7b3036915f9b
MCC676 <676teg@gmail.com>
2024-08-17 19:41:35 -0500

    configure proyect net 6 with angular

---
+++ POS.Infraestruture/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Infraestructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Declaración o matrícula de nuestras interfaces a nivel de repositorio
+        ICategoryRepository Category { get; }
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
---
+++ POS.Infraestruture/Persistences/Repository/CategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Domain.Entities;
+using POS.Infraestructure.Commons.Bases.Request;
+using POS.Infraestructure.Commons.Bases.Response;
+using POS.Infraestructure.Persistences.Contexts;
+using POS.Infraestructure.Persistences.Interfaces;
+using POS.Utilities.Static;
+
+namespace POS.Infraestructure.Persistences.Repository
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(PosContext context) : base(context) { }
+
+        public async Task<BaseEntityResponse<Category>> ListCategories(BaseFiltersRequest filters)
+        {
+            var response = new BaseEntityResponse<Category>();
+
+            var categories = GetEntityQuery(x => x.AuditDeleteUser == null && x.AuditDeleteDate == null);
+
+            if (filters.NumFilter is not null && !string.IsNullOrEmpty(filters.TextFilter))
+            {
+                switch (filters.NumFilter)
+                {
+                    case 1:
+                        categories = categories.Where(x => x.Name!.Contains(filters.TextFilter));
+                        break;
+                    case 2:
+                        categories = categories.Where(x => x.Description!.Contains(filters.TextFilter));
+                        break;
+                }
+            }
+
+            if (filters.StateFilter is not null)
+            {
+                categories = categories.Where(x => x.State.Equals(filters.StateFilter));
+            }
+
+            if(!string.IsNullOrEmpty(filters.StarDate) && !string.IsNullOrEmpty(filters.EndDate))
+            {
+                categories = categories.Where(x => x.AuditCreateDate >= Convert.ToDateTime(filters.StarDate) && x.AuditCreateDate <= Convert.ToDateTime(filters.EndDate).AddDays(1));
+            }
+                                                      
+            if (filters.Sort is null) filters.Sort = "Id";
+
+            response.TotalRecords = await categories.CountAsync();
+            response.Items = await Ordering(filters, categories, !(bool)filters.Download!).ToListAsync();
+            return response;
+        }
+    }
+}
