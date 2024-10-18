c4453285fdd66b7084558eea18694cefc40b1811
MCC676 <676teg@gmail.com>
2024-10-17 23:29:28 -0500

    carga de archivos a repo, configuraciones y mapeo de entidad categorias

--- POS.Infraestruture/obj/Debug/net8.0/POS.Infraestructure.AssemblyInfo.cs
+++ POS.Infraestruture/obj/Debug/net8.0/POS.Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("POS.Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8869313df0766cc3fe995837474b7b3036915f9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("POS.Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("POS.Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
